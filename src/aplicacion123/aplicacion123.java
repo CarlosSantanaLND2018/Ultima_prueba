bf6962306b2a8678d521a59f3b81cd3ffda676d2
AlumGS <AlumGS@PCProfesor>
2019-05-21 19:25:55 +0200

    archivos nuevos

---
+++ src/aplicacion123/aplicacion123.java
@@ -0,0 +1,74 @@
+package aplicacion123;
+
+
+import javax.swing.JOptionPane;
+
+
+public class aplicacion123{
+
+   
+    public static void main(String[] args) {
+        		int operacion;
+        int decision = JOptionPane.showConfirmDialog(null, "¿Quieres calcular algo?");
+		
+        
+    if (decision == JOptionPane.YES_OPTION){
+         operacion = Integer.parseInt(JOptionPane.showInputDialog("Ingrese una opcion: \n 1: SUMA \n 2: RESTA \n 3: MULTIPLICACION \n 4: DIVISION"));
+        if (operacion == 1){
+        	SUMA();
+        }
+        if (operacion == 2){
+        	RESTA();
+        }
+        if (operacion == 3){
+        	MULTIPLICACION();
+        }
+        if (operacion == 4){
+        	 DIVISION();
+        }
+  
+        
+     }
+     if (decision == JOptionPane.NO_OPTION){
+         JOptionPane.showMessageDialog(null, "Fin Del Programa");
+     }
+     if (decision == JOptionPane.CANCEL_OPTION){
+         System.exit(0);
+     }
+ }
+	public static void SUMA() {
+		double num1, num2, resultado;
+		num1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresa un numero: " ));
+        num2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresa otro numero: "));
+        resultado = num1 + num2;
+        JOptionPane.showMessageDialog(null, resultado);
+	}
+        
+	public static void RESTA() {
+		double num1, num2, resultado;
+		 num1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresa un numero: " ));
+         num2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresa otro numero: "));
+         resultado = num1 - num2;
+         JOptionPane.showMessageDialog(null, resultado);
+	}
+        
+	public static void MULTIPLICACION() {
+		double num1, num2, resultado;
+		 num1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresa un numero: " ));
+         num2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresa otro numero: "));
+         resultado = num1 * num2;
+         JOptionPane.showMessageDialog(null, resultado);
+	}
+        
+	public static void DIVISION() {
+		double num1, num2, resultado;
+		num1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresa un numero: " ));
+        num2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresa otro numero: "));
+        resultado = num1 / num2;
+        JOptionPane.showMessageDialog(null, resultado);
+	}
+    
+            
+       
+    }
+    
